b2a39d6366c9f7c70d044191b54a90f2793d5ae7
ronikiko <ronikiko@gmail.com>
2020-06-25 13:00:27 +0300

    Updates

---
+++ precache-manifest.0b4e9dee6e5965762bc7b7969543192d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039778ac7a9ed996c2ef00a3aedef457",
+    "url": "/react-middleProject/index.html"
+  },
+  {
+    "revision": "f8b8791fb1a494759473",
+    "url": "/react-middleProject/static/css/main.2f0d8aa4.chunk.css"
+  },
+  {
+    "revision": "f9d36b717946d1091aab",
+    "url": "/react-middleProject/static/js/2.e8c6a6d2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-middleProject/static/js/2.e8c6a6d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8b8791fb1a494759473",
+    "url": "/react-middleProject/static/js/main.c86069c6.chunk.js"
+  },
+  {
+    "revision": "0c6a926c929a05537c63",
+    "url": "/react-middleProject/static/js/runtime-main.83719734.js"
+  }
+]);
